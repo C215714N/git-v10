1c75464bf147b34a17a903f4a0fca84873d35ea6
Cristian Racedo <CristianDRacedo@hotmail.com>
2022-09-23 12:54:35 -0300

    Logica Menu Hamburguesa

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,12 @@
 window.addEventListener('DOMContentLoaded', () => {
 // Declaraciones
-
+    const d = document;
+    const menuButton = d.querySelector('nav [class*="btn"]');
+    const menuList = d.querySelector('nav .menu');
 // Cabecera
 
 // Navegacion
-
+    menuButton.onclick = () => menuList.classList.toggle('active');
 // Contenido
 
 // Pie de Pagina
