56ed788365720cb5f58824d5e64117f7a9bcb01c
Cristian Racedo <CristianDRacedo@hotmail.com>
2022-10-14 11:14:57 -0300

    Logica Mapa de contacto

--- scripts/index.js
+++ scripts/index.js
@@ -3,12 +3,22 @@ window.addEventListener('DOMContentLoaded', () => {
     const d = document;
     const menuButton = d.querySelector('nav [class*="btn"]');
     const menuList = d.querySelector('nav .menu');
+    const mapButton = d.querySelector('#footer .btn[class*="map-"]');
+    const mapFrame = d.querySelector('#footer iframe.map');
 // Cabecera
 
 // Navegacion
-    menuButton.onclick = () => menuList.classList.toggle('active');
+    menuButton.onclick = toggle(menuButton, menuList, 'btn-open', 'btn-close');
 // Contenido
 
 // Pie de Pagina
+    mapButton.addEventListener('click', (e) => {
+        toggle(e.target, mapFrame, 'map-open', 'map-close' )
+    } )
 
+    function toggle(target, element, a, b){
+        element.classList.toggle('active') ?
+        target.classList.replace(a,b) :
+        target.classList.replace(b,a)
+    }
 }   )
